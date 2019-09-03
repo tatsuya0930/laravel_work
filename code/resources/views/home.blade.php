02ea860913c84b2ba9aca47cc5d8a1a884f4a882
tatsuya0930 <tatsuya.iki@gmail.com>
2019-09-03 23:37:02 +0900

    CSS追加

--- code/resources/views/home.blade.php
+++ code/resources/views/home.blade.php
@@ -1,17 +1,22 @@
 <html>
 <head>
-<meta charset='utf-8'>
+    <meta charset='utf-8'>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-こんにちは！
+<h1>こんにちは！</h1>
 @if (Auth::check())
-    {{ \Auth::user()->name }}さん<br />
-    <a href="/bbs">掲示板</a><br />
-    <a href="/auth/logout">ログアウト</a>
+    <h2>{{ \Auth::user()->name }}さん</h2><br />
+    <div>
+        <a href="/bbs">掲示板</a><br />
+        <a href="/auth/logout">ログアウト</a>
+    </div>
 @else
-    ゲストさん<br />
-    <a href="/auth/login">ログイン</a><br />
-    <a href="/auth/register">会員登録</a>
+    <h2>ゲストさん</h2><br />
+    <div>
+        <a href="/auth/login">ログイン</a><br />
+        <a href="/auth/register">会員登録</a>
+    </div>
 @endif
 </body>
 </html>
